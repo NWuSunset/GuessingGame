a62f65619b9a3cbfcec068ac06e9c515310b9de9
U-BSD\476275 <476275@SHS-9YBF6X3.beaverton.k12.or.us>
2024-08-29 11:45:57 -0700

    Added all functionality, just gotta test it

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -6,7 +6,34 @@ using namespace std;
 
 int main() {
   int srand(time(NULL));
-  int rand()
-  cout << "test";
+  int num = rand() % 100;
+  int input = 0;
+  int guessNum = 0;
+  char answ = 'a';
+  bool gameStatus = true;
+
+
+  while (gameStatus) { 
+  cout << "Guess a number between 0 and 100" << endl;
+  cin >> input;
+  cout << input;
+
+   if (input > num) {
+    cout << "Your number is too high" << endl;
+    guessNum++;
+   } else if (input < num) {
+    cout << "Your number is too low" << endl;
+    guessNum++;
+    }
+    cout << "Good Job you won in" << guessNum << "guesses" << endl;
+    cout << "Do you want to play again?" << "(y/n)" << endl;
+    cin >> answ;
+    cout << answ;
+
+    if (answ == 'n') {
+      gameStatus = false;
+    }
+   
+  } 
   return 0;
 }
